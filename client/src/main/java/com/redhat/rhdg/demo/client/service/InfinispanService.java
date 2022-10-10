2b5df7af502cb5a0b007fb15b63cbae336884752
Trevor Terris <tkterris@gmail.com>
2022-10-10 14:37:34 -0400

    Add authX

--- client/src/main/java/com/redhat/rhdg/demo/client/service/InfinispanService.java
+++ client/src/main/java/com/redhat/rhdg/demo/client/service/InfinispanService.java
@@ -3,18 +3,29 @@ package com.redhat.rhdg.demo.client.service;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ClientIntelligence;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("file:/var/identities.yaml")
 public class InfinispanService {
+	
+	@Autowired
+	Environment env;
 
 	@Bean
 	public RemoteCacheManager getCacheManager() {
 		ConfigurationBuilder cb = new ConfigurationBuilder().addServer()
 				.host("example-infinispan").port(11222)
-				//.host("localhost").port(11222)
+				.security()
+					.authentication()
+						.username(env.getProperty("credentials.username"))
+						.password(env.getProperty("credentials.password"))
 				.clientIntelligence(ClientIntelligence.HASH_DISTRIBUTION_AWARE);
+		System.out.println("Connecting with " + env.getProperty("credentials.username"));
 		return new RemoteCacheManager(cb.build());
 	}
 }
