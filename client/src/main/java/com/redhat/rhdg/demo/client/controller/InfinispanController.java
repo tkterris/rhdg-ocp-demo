57e55ac6b2f24d63769cf7ced86389f31fe52304
Trevor Terris <tkterris@gmail.com>
2022-10-11 09:34:09 -0400

    Parameterized cache name

--- client/src/main/java/com/redhat/rhdg/demo/client/controller/InfinispanController.java
+++ client/src/main/java/com/redhat/rhdg/demo/client/controller/InfinispanController.java
@@ -3,6 +3,7 @@ package com.redhat.rhdg.demo.client.controller;
 import org.infinispan.client.hotrod.Flag;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,29 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/infinispan")
 public class InfinispanController {
-	
-	private static final String CACHE = "test-cache";
+
+	@Value("${cache.name}")
+	private String cache;
 	
 	@Autowired
 	RemoteCacheManager rcm;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public Object[] getAll() {
-		return rcm.getCache(CACHE).keySet().toArray();
+		return rcm.getCache(cache).keySet().toArray();
 	}
 	
 	@RequestMapping(value = "/{key}", method = RequestMethod.GET)
 	public Object get(@PathVariable(name = "key") String key) {
-		return rcm.getCache(CACHE).get(key);
+		return rcm.getCache(cache).get(key);
 	}
 
 	@RequestMapping(value = "/{key}", method = RequestMethod.POST)
 	public Object create(@PathVariable("key") String key, @RequestBody String value) {
-		return rcm.getCache(CACHE).put(key, value);
+		return rcm.getCache(cache).put(key, value);
 	}
 
 	@RequestMapping(value = "/{key}", method = RequestMethod.DELETE)
 	public Object delete(@PathVariable("key") String key) {
-		return rcm.getCache(CACHE).withFlags(Flag.FORCE_RETURN_VALUE).remove(key);
+		return rcm.getCache(cache).withFlags(Flag.FORCE_RETURN_VALUE).remove(key);
 	}
 }
