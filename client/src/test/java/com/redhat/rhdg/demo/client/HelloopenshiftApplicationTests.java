93a02883a1c63dfc97dfe82d05d0fc2032517a87
Trevor Terris <tkterris@gmail.com>
2024-02-01 11:22:53 -0500

    Remove context loading from test
    Fix issue where RHDG cluster must be running in order for tests to pass.

--- client/src/test/java/com/redhat/rhdg/demo/client/HelloopenshiftApplicationTests.java
+++ client/src/test/java/com/redhat/rhdg/demo/client/HelloopenshiftApplicationTests.java
@@ -1,13 +1,8 @@
 package com.redhat.rhdg.demo.client;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloopenshiftApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
